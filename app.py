8770b1b4b01ac6714fec9cfd60119bd64ee4669e
Joe Kokenge <jkokenge@gmail.com>
2014-02-28 11:00:54 -0500

    added detail page for each victim'

--- app.py
+++ app.py
@@ -8,16 +8,23 @@ csv_path = './static/la-riots-deaths.csv'
 
 csv_obj = csv.DictReader(open(csv_path, 'r'))
 csv_list = list(csv_obj)
+csv_dict = dict( [ [o['id'], o ] for o in csv_list ] )
 
 
 
 
 
 @app.route("/")
-
 def index():
 	return render_template('index.html', 
 		object_list = csv_list,
+		)
+
+
+@app.route('/<number>/')
+def detail(number):
+	return render_template('detail.html', 
+		object=csv_dict[number],
 		)
 
 
